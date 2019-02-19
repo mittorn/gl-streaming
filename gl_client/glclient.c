90882a522a1a49f5dbeb8d95d2c7fdf160fabdc9
mittorn <mittorn@sibmail.com>
2019-02-20 04:32:30 +0700

    More stubs

--- gl_client/glclient.c
+++ gl_client/glclient.c
@@ -995,3 +995,11 @@ GL_APICALL void         GL_APIENTRY glBlendFunc (GLenum sfactor, GLenum dfactor)
 {
 
 }
+
+GL_APICALL void         GL_APIENTRY glCullFace (GLenum mode)
+{
+}
+GL_APICALL void         GL_APIENTRY glTexSubImage2D (GLenum target, GLint level, GLint xoffset, GLint yoffset, GLsizei width, GLsizei height, GLenum format, GLenum type, const GLvoid* pixels)
+{
+
+}
